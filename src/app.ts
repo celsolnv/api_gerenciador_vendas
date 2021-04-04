74218f2240bb416de4f966072f0157203ca19933
Celso <celsolnv27@gmail.com>
2021-04-04 19:39:21 -0300

    Adicioando o pacote cors para realizar as restrições de acesso e pegando a porta nas minhas variáveis de ambiente

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,12 @@ import './database';
 import express from "express";
 import {router} from './routes';
 import {createConnection} from 'typeorm';
+import cors from 'cors';
 
 createConnection();
 
 const app = express();
+app.use(cors);
 app.use(express.json());
 app.use(router);
 
