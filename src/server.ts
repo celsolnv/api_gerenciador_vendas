010e456ce08ee4990cf043b08c5b6939e91f69b3
Celso <celsolnv27@gmail.com>
2021-04-05 03:22:13 -0300

    Alteração na porta

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import { app } from "./app"
 
 
-// app.listen( process.env.PORT || 3333,()=>{console.log("Servidor rodando...")})
-app.listen(3333,()=>{console.log("Servidor rodando...")})
+app.listen( process.env.PORT || 3333,()=>{console.log("Servidor rodando...")})
